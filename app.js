d31727c0f9da542cab49966283d74dd87eaea089
m1l4n <milan.holes@riesenia.com>
2015-06-29 22:35:45 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var casper = require('casper').create();
+var express = require('express');
+var app = express();
 
 var original = [];
 var changed_cache = [];
 var changed_url = [];
+
+app.set('port', (process.env.PORT || 5000));
 
 casper.start();
 
@@ -14,3 +18,7 @@ casper.then(function() {
 });
 
 casper.run();
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
