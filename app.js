282dad70da2ac6576978d6d13f06324b571cdb54
m1l4n <milan.holes@riesenia.com>
2015-06-29 22:39:41 +0200

    update

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ var changed_url = [];
 
 app.set('port', (process.env.PORT || 5000));
 
-casper.start();
+// casper.start();
 
-casper.then(function() {
-    this.thenOpen('www.google.sk', function() {
-        this.echo('KONTROLA -> google.sk');
-        console.log('KONTROLA -> google.sk');
-    });
-});
+// casper.then(function() {
+//     this.thenOpen('www.google.sk', function() {
+//         this.echo('KONTROLA -> google.sk');
+//         console.log('KONTROLA -> google.sk');
+//     });
+// });
 
-casper.run();
+// casper.run();
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
