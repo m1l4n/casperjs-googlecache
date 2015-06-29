b549c02d8d3ec1e8d9bc7943dcab613be0475a74
m1l4n <milan.holes@riesenia.com>
2015-06-29 23:19:08 +0200

    update

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-var casper = require('casper').create();
-var fs = require('fs');
-var dname = 'defaults.txt';
-var dsave = fs.pathJoin(fs.workingDirectory, '', dname);
+try {
+    var Spooky = require('spooky');
+} catch (e) {
+    var Spooky = require('../lib/spooky');
+}
 
-casper.start();
+var spooky = new Spooky({
+        child: {
+            transport: 'http'
+        },
+        casper: {
+            logLevel: 'debug',
+            verbose: true
+        }
+    }, function (err) {
+        if (err) {
+            e = new Error('Failed to initialize SpookyJS');
+            e.details = err;
+            throw e;
+        }
 
-casper.then(function() {
-    this.thenOpen('www.google.sk', function() {
-        this.echo('KONTROLA -> google.sk');
-        console.log('KONTROLA -> google.sk');
-        fs.write(dsave, 'adsadsa', 'w');
+        spooky.start(
+            'http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost');
+        spooky.then(function () {
+            this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+                return document.title;
+            }));
+        });
+        spooky.run();
     });
+
+spooky.on('error', function (e, stack) {
+    console.error(e);
+
+    if (stack) {
+        console.log(stack);
+    }
 });
 
-casper.run();
+/*
+// Uncomment this block to see all of the things Casper has to say.
+// There are a lot.
+// He has opinions.
+spooky.on('console', function (line) {
+    console.log(line);
+});
+*/
+
+spooky.on('hello', function (greeting) {
+    console.log(greeting);
+});
+
+spooky.on('log', function (log) {
+    if (log.space === 'remote') {
+        console.log(log.message.replace(/ \- .*/, ''));
+    }
+});
