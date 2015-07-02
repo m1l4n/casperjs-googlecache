3d9c97a5b5e74ac886912fd76a49d694c2a08e3b
m1l4n <milan.holes@riesenia.com>
2015-07-02 16:49:02 +0200

    test

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ try {
 }
 
 var spooky = new Spooky({
-        child: {
-            transport: 'http'
-        },
-        casper: {
-            logLevel: 'debug',
-            verbose: true
-        }
+        // child: {
+        //     transport: 'http'
+        // },
+        // casper: {
+        //     logLevel: 'debug',
+        //     verbose: true
+        // }
     }, function (err) {
         if (err) {
             e = new Error('Failed to initialize SpookyJS');
@@ -59,14 +59,14 @@ spooky.on('error', function (e, stack) {
     }
 });
 
-/*
+
 // Uncomment this block to see all of the things Casper has to say.
 // There are a lot.
 // He has opinions.
 spooky.on('console', function (line) {
     console.log(line);
 });
-*/
+
 
 spooky.on('hello', function (greeting) {
     console.log(greeting);
