15676ebeb09d6aca97cf2cad727bfa57cf875acf
m1l4n <milan.holes@riesenia.com>
2015-07-02 16:40:31 +0200

    test

--- app.js
+++ app.js
@@ -46,13 +46,7 @@ var spooky = new Spooky({
             }));
         });
         spooky.then(function() {
-            this.thenOpen('http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost', function() {
-                console.log('Otvorena stranka');
-                return 'aa';
-                if (this.exists('#google-cache-hdr')) {
-                    console.log(this.getHTML('#google-cache-hdr > div:first-child').replace(/(<([^>]+)>)/ig,""));
-                }
-            });
+            this.emit('test');
         });
         spooky.run();
     });
@@ -76,6 +70,16 @@ spooky.on('console', function (line) {
 
 spooky.on('hello', function (greeting) {
     console.log(greeting);
+});
+
+spooky.on('test', function () {
+    console.log('Spustam test');
+    this.thenOpen('http://en.wikipedia.org/wiki/Spooky_the_Tuff_Little_Ghost', function() {
+        console.log('Otvorena stranka');
+        if (this.exists('#google-cache-hdr')) {
+            console.log(this.getHTML('#google-cache-hdr > div:first-child').replace(/(<([^>]+)>)/ig,""));
+        }
+    });
 });
 
 spooky.on('log', function (log) {
